740e7951d6f28fdb53f072ed062e9a97d7b8db56
Francois POLACK <francois.polack@synchrotron-soleil.fr>
2022-05-20 16:10:09 +0200

    Feat :	resource file added 	shows version info in windows file manager

--- include/version.h
+++ include/version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "16";
+	static const char DATE[] = "20";
 	static const char MONTH[] = "05";
 	static const char YEAR[] = "2022";
 	static const char UBUNTU_VERSION_STYLE[] =  "22.05";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 2;
 	static const long MINOR  = 1;
-	static const long BUILD  = 448;
-	static const long REVISION  = 448;
+	static const long BUILD  = 530;
+	static const long REVISION  = 530;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 1786;
-	#define RC_FILEVERSION 2,1,448,448
-	#define RC_FILEVERSION_STRING "2, 1, 448, 448\0"
-	static const char FULLVERSION_STRING [] = "2.1.448.448";
+	static const long BUILDS_COUNT  = 1905;
+	#define RC_FILEVERSION 2,1,530,530
+	#define RC_FILEVERSION_STRING "2, 1, 530, 530\0"
+	static const char FULLVERSION_STRING [] = "2.1.530.530";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 124;
+	static const long BUILD_HISTORY  = 206;
 	
 
 }
