95896dff6c669f40ce75b9c10462f0d6c5acd224
David DENNETIERE <david.dennetiere@synchrotron-soleil.fr>
2023-04-18 10:52:29 +0200

    merge:merge commit

--- include/version.h
+++ include/version.h
@@ -2,34 +2,32 @@
 #define VERSION_H
 
 namespace AutoVersion{
-
+	
 	//Date Version Types
-
 	static const char DATE[] = "18";
 	static const char MONTH[] = "04";
 	static const char YEAR[] = "2023";
 	static const char UBUNTU_VERSION_STYLE[] =  "23.04";
-
+	
 	//Software Status
 	static const char STATUS[] =  "Alpha";
 	static const char STATUS_SHORT[] =  "a";
-
+	
 	//Standard Version Type
 	static const long MAJOR  = 2;
 	static const long MINOR  = 3;
-
-	static const long BUILD  = 573;
-	static const long REVISION  = 573;
-
+	static const long BUILD  = 460;
+	static const long REVISION  = 460;
+	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 686;
-	#define RC_FILEVERSION 2,3,573,573
-	#define RC_FILEVERSION_STRING "2, 3, 573, 573\0"
-	static const char FULLVERSION_STRING [] = "2.3.573.573";
-
+	static const long BUILDS_COUNT  = 526;
+	#define RC_FILEVERSION 2,3,460,460
+	#define RC_FILEVERSION_STRING "2, 3, 460, 460\0"
+	static const char FULLVERSION_STRING [] = "2.3.460.460";
+	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 436;
-
+	static const long BUILD_HISTORY  = 323;
+	
 
 }
 #endif //VERSION_H
