ddennetiere/optix

commit 24fefd35550d5d5fe7b44db142ac87b399ccee96

Change: ElementBase::getParameter function now returns a C allocated copy of 	the requested parameter. Copy in the external structure provided by 	external callers in now done in GetParameter interface functions.
	EnumerateParameters can deal with array parameters, but array buffer
	are allocated, and are and  must be deleted by C. Array parameter
	must be therefore copied, if they need to be modified.

src/interface.cpp
@@ -578,7 +578,32 @@ extern "C"
                 return false;
             }
             else
-                return ((ElementBase*)elementID)->getParameter(paramTag, *paramData);
+            {
+                Parameter param;
+                if(! ((ElementBase*)elementID)->getParameter(paramTag, param))
+                    return false ;
+                switch(param.copy(*paramData))
+                {
+                case 0:
+                    return true;
+                case 1:
+                    char msg[256];
+                    sprintf(msg, "In Parameter::copy, the array flags of the source struct(%X) and destination struct (%X) do not match",
+                            param.flags, paramData->flags);
+                    SetOptiXLastError(msg,__FILE__, __func__);
+                    return false;
+                case 2:
+                    SetOptiXLastError("In Parameter::copy, the array pointer of the destination Parameter is invalid",__FILE__, __func__);
+                    return false;
+                case 3:
+                    SetOptiXLastError("In Parameter::copy, the data memory pointer of destination is invalid or points to insufficient memory",__FILE__, __func__);
+                    return false;
+                default:
+                    SetOptiXLastError("Invalid return value or Parameter::copy function ",__FILE__, __func__);
+                    return false;
+                }
+
+            }
         }
         else
         {
@@ -672,7 +697,30 @@ extern "C"
                             " is " + std::to_string(paramSize) + ", which is larger than the buffer size "  , __FILE__, __func__);
                 return false;
             }
-            return ((ElementBase*)elementID)->getParameter(paramTag, *paramData);
+//            return ((ElementBase*)elementID)->getParameter(paramTag, *paramData);
+            Parameter param;
+            if(! ((ElementBase*)elementID)->getParameter(paramTag, param))
+                return false ;
+            switch(param.copy(*paramData))
+            {
+            case 0:
+                return true;
+            case 1:
+                char msg[256];
+                sprintf(msg, "In Parameter::copy, the array flags of the source struct(%X) and destination struct (%X) do not match",
+                        param.flags, paramData->flags);
+                SetOptiXLastError(msg,__FILE__, __func__);
+                return false;
+            case 2:
+                SetOptiXLastError("In Parameter::copy, the array pointer of the destination Parameter is invalid",__FILE__, __func__);
+                return false;
+            case 3:
+                SetOptiXLastError("In Parameter::copy, the data memory pointer of destination is invalid or points to insufficient memory",__FILE__, __func__);
+                return false;
+            default:
+                SetOptiXLastError("Invalid return value or Parameter::copy function ",__FILE__, __func__);
+                return false;
+            }
         }
         SetOptiXLastError(string("Single value parameter  ") + paramTag  +
             " should be retrieved with the GetParameter function"  , __FILE__, __func__);
@@ -703,6 +751,15 @@ extern "C"
             SetOptiXLastError("Buffer too small", __FILE__, __func__);
             delete pRef;
             *pHandle=0;
+            if(paramData->flags & ArrayData) // clear Array data if any
+            {
+                if(paramData->paramArray)
+                {
+                    delete paramData->paramArray;
+                    paramData->flags &= ~ArrayData;
+                    paramData->value=0;
+                }
+            }
             return false;
         }
 
@@ -713,14 +770,33 @@ extern "C"
             return true;
         }
         * pHandle=(size_t) pRef;
+        if(paramData->flags & ArrayData) // clear Array data if any
+        {
+            if(paramData->paramArray)
+            {
+                delete paramData->paramArray;
+                paramData->flags &= ~ArrayData;
+                paramData->value=0;
+            }
+        }
         return true;
     }
 
 
-    DLL_EXPORT void ReleaseParameterEnumHandle(size_t handle)
+    DLL_EXPORT void ReleaseParameterEnumHandle(size_t handle, Parameter *paramData)
     {
         if(handle)
             delete (map<string, Parameter>::iterator*) handle;
+        if(paramData)
+            if(paramData->flags & ArrayData) // clear Array data if any
+            {
+                if(paramData->paramArray)
+                {
+                    delete paramData->paramArray;
+                    paramData->flags &= ~ArrayData;
+                    paramData->value=0;
+                }
+            }
     }
 
 

include/elementbase.h
@@ -299,27 +299,8 @@ public:
         ParamIterator it=m_parameters.find(name);
         if (it !=m_parameters.end())
         {
-            switch(it->second.copy(param))
-            {
-            case 0:
-                return true;
-            case 1:
-                char msg[256];
-                sprintf(msg, "In Parameter::copy, the array flags of the source struct(%X) and destination struct (%X) do not match",
-                        it->second.flags, param.flags);
-                SetOptiXLastError(msg,__FILE__, __func__);
-
-                return false;
-            case 2:
-                SetOptiXLastError("In Parameter::copy, the array pointer of the destination Parameter is invalid",__FILE__, __func__);
-                return false;
-            case 3:
-                SetOptiXLastError("In Parameter::copy, the data memory pointer of destination is invalid or points to insufficient memory",__FILE__, __func__);
-                return false;
-            default:
-                SetOptiXLastError("Invalid return value or Parameter::copy function ",__FILE__, __func__);
-                return false;
-            }
+            param=it->second;
+            return true;
         }
         else
         {

include/interface.h
@@ -411,7 +411,9 @@ extern "C"
      *  \n on output, a handle to underlying enumerator of the parameter list or 0 if the retrieved element is the last one
      * \param[out] tagBuffer char* a character buffer to receive the retrieved parameter name.
      * \param[in] bufSize The size of the tag buffer
-     * \param[out] paramData a pointer to a Parameter struct to receive the parameter data
+     * \param[out] paramData a pointer to a Parameter struct to receive the parameter data. This parameter must not have an Array type
+     *      when called for the first time. Array, if any, will be allocated by C and should be copied. They will be cleared by the next
+     *      call to the function
      * \return true if a new parameter was found and returned without error ; false if elementID was invalid or the buffer was too small, and OptixLastError is set
      * in this latter case, the parameter name is truncated and the enumerator is  invalidated  and set to 0
      * Memory leaks will occur if the handle is dropped before it is returned as 0. If needed clear a non zero hande with ReleaseParameterHandle()
@@ -421,9 +423,9 @@ extern "C"
     /** \brief release a parameter enumeration handle returned by EnumerateParameter()
      *
      * \param handle a non null handle returned by EnumerateParameter()
-     *
+     * \param paramData (optional) pointer to the Parameter struct used to iterate. If given will clear paramArray struct created by the iterator
      */
-    DLL_EXPORT void ReleaseParameterEnumHandle(size_t handle);
+    DLL_EXPORT void ReleaseParameterEnumHandle(size_t handle, Parameter *paramData =0);
 
     /** \brief This function defines a polynomial surface to fit the given height data (Only apply to polynomial surfaces)
      *

commit 586ab527f224dd5430d994985f852f6561c25080

fix: bug introduced by commit 24fefd35550d5 in EnumerateParameters function

src/interface.cpp
@@ -767,18 +767,18 @@ extern "C"
         {
             delete pRef;
             *pHandle=0;
+            if(paramData->flags & ArrayData) // clear Array data if any
+            {
+                if(paramData->paramArray)
+                {
+                    delete paramData->paramArray;
+                    paramData->flags &= ~ArrayData;
+                    paramData->value=0;
+                }
+            }
             return true;
         }
         * pHandle=(size_t) pRef;
-        if(paramData->flags & ArrayData) // clear Array data if any
-        {
-            if(paramData->paramArray)
-            {
-                delete paramData->paramArray;
-                paramData->flags &= ~ArrayData;
-                paramData->value=0;
-            }
-        }
         return true;
     }
 