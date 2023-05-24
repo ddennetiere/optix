74b1c092ffc689ec4d8d7bbe5522070326d39c9f
David DENNETIERE <david.dennetiere@synchrotron-soleil.fr>
2023-05-24 09:44:37 +0200

    dev: status version

--- include/version.h
+++ include/version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "12";
+	static const char DATE[] = "23";
 	static const char MONTH[] = "05";
 	static const char YEAR[] = "2023";
 	static const char UBUNTU_VERSION_STYLE[] =  "23.05";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 2;
 	static const long MINOR  = 3;
-	static const long BUILD  = 749;
-	static const long REVISION  = 749;
+	static const long BUILD  = 750;
+	static const long REVISION  = 750;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 960;
-	#define RC_FILEVERSION 2,3,749,749
-	#define RC_FILEVERSION_STRING "2, 3, 749, 749\0"
-	static const char FULLVERSION_STRING [] = "2.3.749.749";
+	#define RC_FILEVERSION 2,3,750,750
+	#define RC_FILEVERSION_STRING "2, 3, 750, 750\0"
+	static const char FULLVERSION_STRING [] = "2.3.750.750";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 612;
+	static const long BUILD_HISTORY  = 613;
 	
 
 }
